c62f2b251c86316dac595e17352d4aee92f7d8d6
mariosknl <mariosknl@outlook.com>
2022-02-08 17:40:03 +0200

    created header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 const Header = () => {
   return (
@@ -17,6 +18,20 @@ const Header = () => {
           placeholder="Collection, item or user...."
         />
       </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} />
+        </div>
+      </div>
+
+      <div className="loginButton">GET IN</div>
     </div>
   );
 };

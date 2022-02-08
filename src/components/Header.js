59ed72af4087456c8ea4f4360c371e30723b1491
mariosknl <mariosknl@outlook.com>
2022-02-08 15:03:20 +0200

    building the header components

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} />
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user...."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
